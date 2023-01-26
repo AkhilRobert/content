fb4180f1dd26010b859afabda9f4e5ab88b0f20b
Akhil Robert <akhilrobert2412@gmail.com>
2023-01-26 13:45:46 +0530

    mvp

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 # FIXME: Change the collection name to valid one on deploy
 import html
+import math
 from typing import Union
 
 from bson import json_util
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 
@@ -51,6 +52,43 @@ map = {
     "Uttar Pradesh": "in-up",
     "Uttarakhand": "in-ut",  # Uttaranchal -> Uttarakhand
     "Jharkhand": "in-jh",
+}
+
+population = {
+    "Puducherry": 808_117,
+    "Lakshadweep": 52_820,
+    "West Bengal": 68_077_970,
+    "Odisha": 31_659_740,  # Orissa -> Odisha
+    "Bihar": 64_531_200,
+    "Sikkim": 406_000,
+    "Chhattisgarh": 17_615_600,
+    "Tamil Nadu": 55_859_300,
+    "Madhya Pradesh": 48_566_800,
+    "Gujarat": 41_309_580,
+    "Goa": 1_170_115,
+    "Nagaland": 1_210_492,
+    "Manipur": 1_837_900,
+    "Arunachal Pradesh": 865_900,
+    "Mizoram": 690_963,
+    "Tripura": 2_757_205,
+    "Daman and Diu": 102_110,
+    "Delhi (Ut)": 9_421_311,
+    "Haryana": 16_464_600,
+    "Chandigarh": 642_374,
+    "Himachal Pradesh": 5_170_877,
+    "Jammu & Kashmir": 7_718_700,
+    "Kerala": 29_098_523,
+    "Karnataka": 44_977_200,
+    "Dadra and Nagar Haveli": 138_290,
+    "Maharashtra": 78_937_190,
+    "Assam": 22_414_320,
+    "Andhra Pradesh": 66_508_170,
+    "Meghalaya": 1_774_778,
+    "Punjab": 20_281_971,
+    "Rajasthan": 44_005_990,
+    "Uttar Pradesh": 132_062_800,
+    "Uttarakhand": 7_051_600,
+    "Jharkhand": 21_844_550,
 }
 
 
@@ -197,6 +235,38 @@ def get_by_age_group(state: str):
         ]
     )
     return json_util.dumps(data)
+
+
+@app.route(
+    "/api/predict/<string:state>",
+    methods=("POST", "GET"),
+)
+def get_prediction(state: str):
+
+    if request.json is None:
+        return {"percentage": 1}
+
+    data = collection.aggregate(
+        [
+            {
+                "$match": {
+                    "State": html.unescape(state),
+                    "Type": request.json["profession"],
+                    "Gender": request.json["gender"],
+                },
+            },
+            {
+                "$group": {
+                    "_id": "$State",
+                    "count": {"$sum": "$Total"},
+                },
+            },
+        ]
+    )
+
+    data = list(data)
+    count = data[0]["count"]
+    return {"percentage": math.ceil((count / population[html.unescape(state)]) * 100)}
 
 
 if __name__ == "__main__":
